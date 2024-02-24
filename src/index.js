bc148f46315282617b80f2feac2dc1876fa22e86
Sajal Sangal <sajalsangal@gmail.com>
2024-02-24 18:54:57 +0530

    created index file, initialized package json and created a webpack, then initiated firebase db and recovered info from it

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8exevj5__ljrfGtpRYWPOwzcvcHJS6v4",
+    authDomain: "demoproject-98ad3.firebaseapp.com",
+    projectId: "demoproject-98ad3",
+    storageBucket: "demoproject-98ad3.appspot.com",
+    messagingSenderId: "526460598177",
+    appId: "1:526460598177:web:7de83db580164260343de0"
+  };
+
+var app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+const colRef = collection(db, 'Anime')
+
+getDocs(colRef).then((snapshot) => {
+    let anime = []
+
+    snapshot.docs.forEach((doc) => {
+        anime.push({ ...doc.data(), id: doc.id });
+    })
+
+    console.log(anime);
+})
+.catch(err => {
+    console.log(err.message)
+})
